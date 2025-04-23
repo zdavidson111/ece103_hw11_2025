fce45f20fe6318eef249b187a69b91180834ee16
Aleksander Malinowski <olekmali@bradley.edu>
2025-04-23 15:37:35 -0500

    Daily update by DrAM

--- ece103_hw11_2025/external_data.h
+++ ece103_hw11_2025/external_data.h
@@ -6,8 +6,8 @@
 #ifndef _external_data_h
 #define _external_data_h
 
-constexpr char homework_version[] = "3.03";
-// This is the version of the homework project updated on April 22, 2025
+constexpr char homework_version[] = "3.04";
+// This is the version of the homework project updated on April 23, 2025
 
 // The instrctor will do his best to update this file every day until the assignment is due.
 
